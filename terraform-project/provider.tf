a26327ca23586c8f311d2e82c8bcc65a9c62a421
Sourabh-Haldar <107604525+Sourabh-Haldar@users.noreply.github.com>
2024-02-21 16:45:17 +0530

    Add files via upload

---
+++ terraform-project/provider.tf
@@ -0,0 +1,17 @@
+
+
+terraform {
+required_providers {
+aws = {
+source  = "hashicorp/aws"
+version = "~> 3.0"
+}
+}
+}
+
+provider "aws" {
+region = "us-east-1"
+access_key = "ASIA6BBJJCIHDR5ANVGQ"
+secret_key = "yBlTxpdOrVmZFxOSKABJO48cl/XHoMFmjTEun1Eo"
+token = "FwoGZXIvYXdzEGwaDI5F0YtU4biMH7+6ACKvAQxNUTLKgfLGJpaAV5KxSt4DHJNLFCzyxqAX/ka5pHpgL7298+wng6jPNwyxyCFqG9K4I9b5LMRp19IHO9JM6QOSS7XGvyc2Y1/7+sRrEK+V1QzYqT0tCjU0UrxJvUBnbZsLIjfm2FYCftoFSmB7MJR2ixaAZg2ahxlf+s/nQ7CQoccESPGE36j4ESHsYGTRnYDAqijCkqW1PN5s+fmOwH1mSzXqd1MKQ+W2m+23n8souKjXrgYyLdMI91QFfEbQarsx5FJmSVLp34kc0SsYsZ28ZvkbGPKxQfUQirKWu374eGM3Dw=="
+}
